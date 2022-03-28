f5d94f1c560b71d9f557979f147b72b305cca5da
Leer0r <avedd72@gmail.com>
2022-03-29 01:33:27 +0200

    Major update

--- scripts/JS/debianOs.js
+++ scripts/JS/debianOs.js
@@ -1,28 +1,132 @@
 var DebianOS = /** @class */ (function () {
-    function DebianOS(navbarHook, desktopHook, osMenuPannel) {
+    function DebianOS(navbarHook, desktopHook, osMenuPannelHook, windowsContainerHook) {
         if (navbarHook === void 0) { navbarHook = "#navbar"; }
         if (desktopHook === void 0) { desktopHook = "#desktop"; }
-        if (osMenuPannel === void 0) { osMenuPannel = "#osMenuPannelContainer"; }
+        if (osMenuPannelHook === void 0) { osMenuPannelHook = desktopHook + " #osMenuPannelContainer"; }
+        if (windowsContainerHook === void 0) { windowsContainerHook = desktopHook + " #windowsContainer"; }
         console.log("init class constructor");
         this.setnavBar(navbarHook);
         this.setDesktop(desktopHook);
-        this.setOSMenu();
-        this.addTestApp();
-        this.setDesktopBackground();
+        this.setOsMenuPannel(osMenuPannelHook);
+        this.setWindowsContainer(windowsContainerHook);
     }
+    //Initial set
     DebianOS.prototype.setnavBar = function (navbarHook) {
         this.navBar = document.querySelector(navbarHook);
     };
     DebianOS.prototype.setDesktop = function (desktopHook) {
+        this.isDown = false;
         this.desktop = document.querySelector(desktopHook);
+        this.setDesktopBackground();
+        this.enableDocumentHandle();
+    };
+    DebianOS.prototype.enableDocumentHandle = function () {
+        var _this = this;
+        document.addEventListener("mouseup", function () {
+            _this.isDown = false;
+        }, true);
+        document.addEventListener("mousemove", function (event) {
+            event.preventDefault();
+            if (_this.isDown) {
+                var mousePosition = {
+                    x: event.clientX,
+                    y: event.clientY
+                };
+                _this.currentFocusedWindow.style.left = (mousePosition.x + _this.offset[0]) + 'px';
+                _this.currentFocusedWindow.style.top = (mousePosition.y + _this.offset[1]) + 'px';
+            }
+        });
     };
     DebianOS.prototype.setOsMenuPannel = function (osMenuHook) {
-        this.osMenuPanel = document.querySelector(osMenuHook);
+        this.osMenuPannel = document.querySelector(osMenuHook);
+        this.setOSMenu();
+        this.setOsMenuPannelContent();
+    };
+    DebianOS.prototype.setWindowsContainer = function (windowsContainerHook) {
+        this.windowsContainer = document.querySelector(windowsContainerHook);
+        var testWindow = {
+            description: "",
+            logo: "url(../../ressources/app/Discord-Logo.png)",
+            name: "discord"
+        };
+        this.createApp(testWindow);
+        this.createApp(testWindow);
     };
     //Desktop functions
     DebianOS.prototype.setDesktopBackground = function () {
         var backGroundUrl = "url(../../ressources/os/kali-layers-16x9.png)";
         this.desktop.style.backgroundImage = backGroundUrl;
+    };
+    DebianOS.prototype.setOsMenuPannelContent = function () {
+        console.log(this.osMenuPannel.children);
+        this.setOsMenuPannelContentUserAccount(this.osMenuPannel.children[0]);
+        this.setOsMenuPannelContentSearchBar(this.osMenuPannel.children[1]);
+        this.setOsMenuPannelContentleftSubPannel(this.osMenuPannel.children[2]);
+        this.setOsMenuPannelContentrightSubPannel(this.osMenuPannel.children[3]);
+        this.setOsMenuPannelContentbottomSubPannel(this.osMenuPannel.children[4]);
+    };
+    DebianOS.prototype.setOsMenuPannelContentUserAccount = function (target) {
+        var userLogo = target.children[0];
+        var userName = target.children[1];
+    };
+    DebianOS.prototype.setOsMenuPannelContentSearchBar = function (target) {
+    };
+    DebianOS.prototype.setOsMenuPannelContentleftSubPannel = function (target) {
+    };
+    DebianOS.prototype.setOsMenuPannelContentrightSubPannel = function (target) {
+    };
+    DebianOS.prototype.setOsMenuPannelContentbottomSubPannel = function (target) {
+    };
+    DebianOS.prototype.createApp = function (application) {
+        this.createAppWindow(application);
+        this.addAppNavBar(application);
+        console.log();
+    };
+    DebianOS.prototype.createAppWindow = function (application) {
+        var window = document.createElement("div");
+        window.classList.add("window");
+        var topBar = document.createElement("div");
+        topBar.classList.add("topBar");
+        var appLogo = document.createElement("div");
+        appLogo.classList.add("appLogo");
+        appLogo.style.backgroundImage = application.logo;
+        topBar.appendChild(appLogo);
+        var appName = document.createElement("div");
+        appName.classList.add("appName");
+        appName.innerHTML = application.name;
+        topBar.appendChild(appName);
+        var navButton = document.createElement("div");
+        navButton.classList.add("navButton");
+        topBar.appendChild(navButton);
+        window.appendChild(topBar);
+        var core = document.createElement("div");
+        core.classList.add("core");
+        window.appendChild(core);
+        console.log(window);
+        this.setupWindowEventListener(window);
+        this.windowsContainer.appendChild(window);
+    };
+    DebianOS.prototype.setupWindowEventListener = function (window) {
+        var _this = this;
+        var topBar = window.children[0];
+        // topBar.addEventListener("click", (ev:MouseEvent) => {
+        //     alert("coucou")
+        // })
+        topBar.addEventListener("mousedown", function (ev) {
+            if (_this.currentFocusedWindow != undefined) {
+                _this.currentFocusedWindow.style.zIndex = "0";
+            }
+            _this.currentFocusedWindow = window;
+            _this.currentFocusedWindow.style.zIndex = "20";
+            _this.mouseDownEvent(ev);
+        }, true);
+    };
+    DebianOS.prototype.mouseDownEvent = function (event) {
+        this.isDown = true;
+        this.offset = [
+            this.currentFocusedWindow.offsetLeft - event.clientX,
+            this.currentFocusedWindow.offsetTop - event.clientY
+        ];
     };
     //NavBar functions 
     DebianOS.prototype.setOSMenu = function () {
@@ -31,7 +135,7 @@ var DebianOS = /** @class */ (function () {
         };
         this.addOsMenu(this.osMenu);
     };
-    DebianOS.prototype.addApp = function (app) {
+    DebianOS.prototype.addAppNavBar = function (app) {
         var osAppDiv = document.createElement("div");
         osAppDiv.classList.add("appContainer");
         osAppDiv.classList.add("appDesign");
@@ -39,15 +143,25 @@ var DebianOS = /** @class */ (function () {
         this.navBar.appendChild(osAppDiv);
     };
     DebianOS.prototype.addOsMenu = function (osMenuInfo) {
+        var _this = this;
         var osMenuDiv = document.createElement("div");
         osMenuDiv.id = "osMenuDesign";
         osMenuDiv.classList.add("appContainer");
         osMenuDiv.style.backgroundImage = osMenuInfo.logo;
-        osMenuDiv.addEventListener("click", this.toogleMenu);
+        osMenuDiv.addEventListener("click", function () {
+            _this.toogleMenu();
+        });
         this.navBar.appendChild(osMenuDiv);
     };
     DebianOS.prototype.toogleMenu = function () {
-        alert("coucou");
+        if (this.osMenuPannel.style.height == "" || this.osMenuPannel.style.height == "0%") {
+            this.osMenuPannel.style.height = "60%";
+            this.osMenuPannel.style.borderBottom = "solid 2px grey";
+        }
+        else {
+            this.osMenuPannel.style.height = "0%";
+            this.osMenuPannel.style.borderBottom = "none";
+        }
     };
     DebianOS.prototype.addTestApp = function () {
         var appDiv = {
@@ -55,7 +169,7 @@ var DebianOS = /** @class */ (function () {
             logo: "url(../../ressources/app/Discord-Logo.png)",
             name: "testApp"
         };
-        this.addApp(appDiv);
+        this.addAppNavBar(appDiv);
     };
     return DebianOS;
 }());
--- scripts/TS/debianOs.ts
+++ scripts/TS/debianOs.ts
@@ -1,7 +1,8 @@
 interface application{
     name:string,
     logo:string,
-    description:string
+    description:string,
+    link?:string
 }
 
 interface osMenu{
@@ -10,33 +11,76 @@ interface osMenu{
 
 class DebianOS
 {
-    navBar: HTMLElement;
-    desktop: HTMLElement;
+    navBar: HTMLDivElement;
+    desktop: HTMLDivElement;
     osMenu: osMenu;
-    osMenuPanel: HTMLElement;
+    osMenuPannel: HTMLDivElement;
+    windowsContainer: HTMLDivElement;
+    isDown:boolean;
+    currentFocusedWindow:HTMLDivElement;
+    offset: number[];
+    desktopManager:[HTMLDivElement,HTMLDivElement]
 
     constructor(navbarHook:string="#navbar", 
                 desktopHook:string="#desktop",
-                osMenuPannel:string="#osMenuPannelContainer"){
+                osMenuPannelHook:string=desktopHook+" #osMenuPannelContainer",
+                windowsContainerHook:string=desktopHook+" #windowsContainer"){
         console.log("init class constructor")
         
         this.setnavBar(navbarHook);
         this.setDesktop(desktopHook);
-        this.setOSMenu();
-        this.addTestApp();
-        this.setDesktopBackground();
+        this.setOsMenuPannel(osMenuPannelHook);
+        this.setWindowsContainer(windowsContainerHook);
     }
+
+    //Initial set
 
     setnavBar(navbarHook:string) {
         this.navBar = document.querySelector(navbarHook);
     }
 
     setDesktop(desktopHook:string){
+        this.isDown = false;
         this.desktop = document.querySelector(desktopHook);
+        this.setDesktopBackground();
+        this.enableDocumentHandle()
+    }
+
+    enableDocumentHandle(){
+        document.addEventListener("mouseup", () => {
+            this.isDown = false;
+        },true)
+
+        document.addEventListener("mousemove", (event:MouseEvent) => {
+            event.preventDefault();
+            if(this.isDown){
+                const mousePosition = {
+
+                    x : event.clientX,
+                    y : event.clientY
+        
+                };
+                this.currentFocusedWindow.style.left = (mousePosition.x + this.offset[0]) + 'px';
+                this.currentFocusedWindow.style.top  = (mousePosition.y + this.offset[1]) + 'px';
+            }
+        })
     }
 
     setOsMenuPannel(osMenuHook:string){
-        this.osMenuPanel = document.querySelector(osMenuHook);
+        this.osMenuPannel = document.querySelector(osMenuHook);
+        this.setOSMenu();
+        this.setOsMenuPannelContent()
+    }
+
+    setWindowsContainer(windowsContainerHook:string){
+        this.windowsContainer = document.querySelector(windowsContainerHook);
+        const testWindow:application = {
+            description:"",
+            logo: "url(../../ressources/app/Discord-Logo.png)",
+            name: "discord"
+        }
+        this.createApp(testWindow);
+        this.createApp(testWindow);
     }
 
     //Desktop functions
@@ -46,6 +90,101 @@ class DebianOS
         this.desktop.style.backgroundImage = backGroundUrl;
     }
 
+    setOsMenuPannelContent(){
+        console.log(this.osMenuPannel.children);
+        this.setOsMenuPannelContentUserAccount(<HTMLDivElement>this.osMenuPannel.children[0])
+        this.setOsMenuPannelContentSearchBar(<HTMLDivElement>this.osMenuPannel.children[1]);
+        this.setOsMenuPannelContentleftSubPannel(<HTMLDivElement>this.osMenuPannel.children[2])
+        this.setOsMenuPannelContentrightSubPannel(<HTMLDivElement>this.osMenuPannel.children[3])
+        this.setOsMenuPannelContentbottomSubPannel(<HTMLDivElement>this.osMenuPannel.children[4])
+    }
+
+    setOsMenuPannelContentUserAccount(target:HTMLElement){
+        const userLogo = target.children[0];
+        const userName = target.children[1];
+
+    }
+
+    setOsMenuPannelContentSearchBar(target:HTMLElement){
+
+    }
+
+    setOsMenuPannelContentleftSubPannel(target:HTMLElement){
+
+    }
+    
+    setOsMenuPannelContentrightSubPannel(target:HTMLElement){
+
+    }
+
+    setOsMenuPannelContentbottomSubPannel(target:HTMLElement){
+
+    }
+
+    createApp(application:application){
+        this.createAppWindow(application);
+        this.addAppNavBar(application);
+        console.log()
+    }
+
+    createAppWindow(application:application){
+        const window = document.createElement("div");
+        window.classList.add("window")
+
+        const topBar = document.createElement("div");
+        topBar.classList.add("topBar")
+
+        const appLogo = document.createElement("div");
+        appLogo.classList.add("appLogo");
+        appLogo.style.backgroundImage = application.logo
+        topBar.appendChild(appLogo)
+
+        const appName = document.createElement("div");
+        appName.classList.add("appName")
+        appName.innerHTML = application.name
+        topBar.appendChild(appName)
+
+        const navButton = document.createElement("div");
+        navButton.classList.add("navButton")
+        topBar.appendChild(navButton)
+
+        window.appendChild(topBar);
+        
+        const core = document.createElement("div")
+        core.classList.add("core");
+        window.appendChild(core)
+        console.log(window);
+        this.setupWindowEventListener(window)
+        
+        this.windowsContainer.appendChild(window)
+
+
+    }
+
+    setupWindowEventListener(window:HTMLDivElement){
+        const topBar = window.children[0];
+        // topBar.addEventListener("click", (ev:MouseEvent) => {
+        //     alert("coucou")
+        // })
+        topBar.addEventListener("mousedown", (ev:MouseEvent) => {
+            if(this.currentFocusedWindow != undefined) {
+                this.currentFocusedWindow.style.zIndex = "0"
+            }
+            this.currentFocusedWindow = window;
+            this.currentFocusedWindow.style.zIndex = "20";
+            this.mouseDownEvent(ev)
+        },true
+        )
+    }
+    mouseDownEvent(event:MouseEvent){
+        this.isDown = true;
+        this.offset = [
+            this.currentFocusedWindow.offsetLeft - event.clientX,
+            this.currentFocusedWindow.offsetTop - event.clientY
+        ];
+    }
+
+    
     //NavBar functions 
 
     setOSMenu(){
@@ -56,7 +195,7 @@ class DebianOS
         this.addOsMenu(this.osMenu);
     }
 
-    addApp(app:application) {
+    addAppNavBar(app:application) {
         let osAppDiv:HTMLDivElement = document.createElement("div");
         osAppDiv.classList.add("appContainer")
         osAppDiv.classList.add("appDesign")
@@ -69,11 +208,21 @@ class DebianOS
         osMenuDiv.id = "osMenuDesign"
         osMenuDiv.classList.add("appContainer")
         osMenuDiv.style.backgroundImage = osMenuInfo.logo;
-        osMenuDiv.addEventListener("click",this.toogleMenu);
+        osMenuDiv.addEventListener("click",() => {
+            this.toogleMenu()
+        });
         this.navBar.appendChild(osMenuDiv)
     }
     toogleMenu() {
-        alert("coucou")
+        if(this.osMenuPannel.style.height == "" || this.osMenuPannel.style.height == "0%") {
+            this.osMenuPannel.style.height = "60%"
+            this.osMenuPannel.style.borderBottom = "solid 2px grey"
+        }
+        else {
+            this.osMenuPannel.style.height = "0%"
+            this.osMenuPannel.style.borderBottom = "none"
+        }
+    
     }
 
     addTestApp(){
@@ -83,7 +232,7 @@ class DebianOS
             name: "testApp"
         }
 
-        this.addApp(appDiv);
+        this.addAppNavBar(appDiv);
     }
 }
 
